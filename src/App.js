abbd459f581423a0de8e70dfdfeb620341a74cee
Jonty-Lees <lees.jonty@gmail.com>
2021-11-17 23:02:02 +0000

    Added routes and created function 2 add to wl list

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 /* eslint-disable */
 
-import React, { useEffect, useState } from "react";
-import MovieContainer from "./components/MovieContainer";
-import Header from "./components/Header"
-import SearchBar from './components/Searchbar'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./styles.css";
+import React from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './Home'
+import WatchLater from "./WatchLater";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("")
+  return (
+    <div>
+      <Routes>
+        <Route exact path='/' element={
+          <Home />
+        } />
+        <Route path='/watchlater' element={
+          <WatchLater />
+        } />
 
-  
-  // API CALL
+      </Routes>
 
-  const axios = require("axios");
-
-  useEffect(() => {
-    async function apiCall(searchValue) {
-      const response = await axios.get(`http://www.omdbapi.com/?s=${searchValue}&apikey=9c35788e`);
-
-      if(response.data.Search) {
-        setMovies(response.data.Search);
-      }
-    }
-    apiCall(searchValue);
-  }, [searchValue]);
-
-
-  return <div>
-    <Header />
-    <div className='container-fluid movie-app'>
-      <div>
-        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
-        <div className='row'>
-          <MovieContainer movies={movies} />
-      </div>
-    </div>
-
-  </div>
+    </div>)
 }
 
 export default App
