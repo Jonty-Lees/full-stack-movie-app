64c83157140a2d4bb4ed8b601b88e7a5e9d60bd8
Jonty-Lees <lees.jonty@gmail.com>
2021-11-19 13:36:51 +0000

    css styled nav links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,8 +5,8 @@ function Navigation() {
   return (
     <div className="nav">
       <ul role="nav">
-        <span><Link to="/">Home</Link></span>
-        <span><Link to="/watchlater">Watch Later</Link></span>
+        <span><Link to="/" id="nav-link">Home</Link></span>
+        <span><Link to="/watchlater" id="nav-link" >Watch Later</Link></span>
       </ul>
     </div>
   );
